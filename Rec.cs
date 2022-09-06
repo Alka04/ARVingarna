3854e6d6c24a452faee95162be17a92f3ddf26bb
Alka04 <alka04006@utb.vaxjo.se>
2022-09-06 11:20:43 +0200

    Rect

---
+++ Rec.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    class Rec: Shape
+    {
+        public Rec(int width, int height ) : base(30)
+        {
+
+        }
+    }
